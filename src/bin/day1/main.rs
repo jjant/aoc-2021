2e5336a36b8368302da18e08d4ccc2b672759cc5
jjant <jantonielli@itba.edu.ar>
2021-12-02 22:50:43 +0000

    Day 1 - Part 2

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,18 +1,31 @@
-fn main() {
-    let input_file = include_str!("input.txt");
+fn count_contiguous_increments(sequence: &[usize]) -> usize {
+    sequence.windows(2).fold(0, |result, windows| {
+        result + (windows[0] < windows[1]) as usize
+    })
+}
 
+fn part1(measurements: &[usize]) -> usize {
+    count_contiguous_increments(&measurements)
+}
+
+fn part2(measurements: &[usize]) -> usize {
+    let summed_windows = measurements
+        .windows(3)
+        .map(|window| window.iter().sum())
+        .collect::<Vec<usize>>();
+
+    count_contiguous_increments(&summed_windows)
+}
+
+fn main() {
+    let input_file: &str = include_str!("input.txt");
     let measurements = input_file
         .lines()
         .map(|line| line.parse().unwrap())
         .collect::<Vec<usize>>();
 
-    let mut result = 0;
-    for following_measurements in measurements.windows(2) {
-        let today = following_measurements[0];
-        let tomorrow = following_measurements[1];
-
-        result += (today < tomorrow) as i32;
-    }
-
-    println!("{:?}", result);
+    println!("Part 1:");
+    println!("\t{}", part1(&measurements));
+    println!("Part 2:");
+    println!("\t{}", part2(&measurements));
 }
