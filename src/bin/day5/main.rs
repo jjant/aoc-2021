8f08f5ae23cea5211a8c85ed922b275e0b654c0a
jjant <jantonielli@itba.edu.ar>
2021-12-05 23:40:44 +0000

    Day 5 - Part 2

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -1,6 +1,6 @@
-use std::vec;
+use std::{cmp::Ordering, vec};
 
-struct World(Vec<Vec<u32>>);
+struct World(Vec<Vec<i32>>);
 
 impl World {
     pub fn new(width: usize, height: usize) -> Self {
@@ -31,25 +31,25 @@ impl World {
     }
 }
 
-fn part1(lines: &[Line]) -> u32 {
+fn solve(lines: &[Line], include_diagonals: bool) -> i32 {
     let width = lines
         .iter()
         .map(|line| line.start.x.max(line.end.x))
         .max()
-        .unwrap() as usize
+        .unwrap_or(0) as usize
         + 1;
     let height = lines
         .iter()
         .map(|line| line.start.y.max(line.end.y))
         .max()
-        .unwrap() as usize
+        .unwrap_or(0) as usize
         + 1;
 
     let mut world = World::new(width, height);
 
     for line in lines {
-        for point in line.iter() {
-            world.mark(&point)
+        for point in line.iter(include_diagonals) {
+            world.mark(&point);
         }
     }
 
@@ -65,7 +65,13 @@ fn part1(lines: &[Line]) -> u32 {
     overlap_count
 }
 
-// fn part2(numbers_to_draw: &[u32], boards: &mut Vec<Board>) -> u32 {}
+fn part1(lines: &[Line]) -> i32 {
+    solve(lines, false)
+}
+
+fn part2(lines: &[Line]) -> i32 {
+    solve(lines, true)
+}
 
 fn main() {
     let input_file: &str = include_str!("input.txt");
@@ -75,21 +81,23 @@ fn main() {
         .map(|line| Line::parse(line))
         .collect::<Option<Vec<_>>>();
 
+    let lines = parsed_input.unwrap();
+
     println!("Part 1:");
-    println!("\t{}", part1(&parsed_input.unwrap()));
+    println!("\t{}", part1(&lines));
     println!("Part 2:");
-    // println!("\t{}", part2(&parsed_input.0, &mut parsed_input.1));
+    println!("\t{}", part2(&lines));
 }
 
 #[derive(Debug)]
 struct Point {
-    x: u32,
-    y: u32,
+    x: i32,
+    y: i32,
 }
 
 impl Point {
-    // Parses a string like:
-    //  "0,9"
+    /// Parses a string like:
+    ///  "0,9"
     pub fn parse(input: &str) -> Option<Self> {
         let (str_x, str_y) = input.split_once(',')?;
 
@@ -98,53 +106,74 @@ impl Point {
 
         Some(Self { x, y })
     }
+
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.x.cmp(&other.x) {
+            Ordering::Equal => self.y.cmp(&other.y),
+            ordering => ordering,
+        }
+    }
+
+    pub fn sort2(a: Point, b: Point) -> (Point, Point) {
+        match a.cmp(&b) {
+            Ordering::Less => (a, b),
+            Ordering::Equal => (a, b),
+            Ordering::Greater => (b, a),
+        }
+    }
 }
+
+/// Lines assume sorted points.
 #[derive(Debug)]
 struct Line {
+    /// "Top-left" point
     start: Point,
+    /// "Bottom-right" point
     end: Point,
 }
 
 impl Line {
-    // Parses a string like:
-    //  "0,9 -> 5,9"
+    /// Parses a string like:
+    ///  "0,9 -> 5,9"
     pub fn parse(input: &str) -> Option<Self> {
         let (str_p1, str_p2) = input.split_once(" -> ")?;
 
-        let start = Point::parse(str_p1)?;
-        let end = Point::parse(str_p2)?;
+        let p1 = Point::parse(str_p1)?;
+        let p2 = Point::parse(str_p2)?;
+
+        let (start, end) = Point::sort2(p1, p2);
 
         Some(Self { start, end })
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = Point> {
+    pub fn iter(&self, include_diagonals: bool) -> impl Iterator<Item = Point> {
         let is_vertical = self.start.x == self.end.x;
         let is_horizontal = self.start.y == self.end.y;
         let mut vec = vec![];
 
-        // We do the two checks to ignore diagonal lines.
+        // We do the two checks to ignore diagonal lines (part1).
         if is_vertical {
-            let (min_y, max_y) = sort2(self.start.y, self.end.y);
-
-            for y in min_y..=max_y {
+            for y in self.start.y..=self.end.y {
                 vec.push(Point { x: self.start.x, y })
             }
         } else if is_horizontal {
-            let (min_x, max_x) = sort2(self.start.x, self.end.x);
-
-            for x in min_x..=max_x {
+            for x in self.start.x..=self.end.x {
                 vec.push(Point { x, y: self.start.y })
+            }
+        } else if include_diagonals {
+            // Diagonal (45 degree) case
+            let dx = self.end.x - self.start.x;
+            let dy = self.end.y - self.start.y;
+            let slope = dy / dx;
+
+            for x in self.start.x..=self.end.x {
+                vec.push(Point {
+                    x,
+                    y: self.start.y + slope * (x - self.start.x),
+                })
             }
         }
 
         vec.into_iter()
-    }
-}
-
-fn sort2(a: u32, b: u32) -> (u32, u32) {
-    if a < b {
-        (a, b)
-    } else {
-        (b, a)
     }
 }
