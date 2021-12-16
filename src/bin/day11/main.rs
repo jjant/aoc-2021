d3df7899d53c365350a044e5f263cb31d52bf0a2
jjant <jantonielli@itba.edu.ar>
2021-12-16 19:02:20 +0000

    Day 11 - Part 2

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 struct Energy {
     level: u32,
     flashed: bool,
@@ -11,119 +11,111 @@ struct Grid {
 }
 
 impl Grid {
-    fn new(width: usize, height: usize) -> Self {
-        let energy = Energy {
-            level: 0,
-            flashed: false,
-        };
-        let grid = vec![vec![energy; width]; height];
-
-        Self {
-            grid,
-            width,
-            height,
-        }
-    }
-
     fn step(&mut self) -> u32 {
         let width = self.width;
         let height = self.height;
 
         for x in 0..width {
             for y in 0..height {
-                self.inc(x, y)
+                self.inc(x as isize, y as isize)
             }
         }
 
         let mut res = 0;
         for x in 0..width {
             for y in 0..height {
-                res += self.reset_flashed(x, y);
+                res += self.reset_flashed(x as isize, y as isize);
             }
         }
 
         res
     }
 
-    fn flash(&mut self, x: usize, y: usize) {
-        self.grid[y][x].flashed = true;
+    fn flash(&mut self, x: isize, y: isize) {
+        if let Some(e) = self.get_mut(x, y) {
+            e.flashed = true;
 
-        for (x, y) in self.neighbors(x, y) {
-            self.inc(x, y)
+            for (x, y) in self.neighbors(x, y) {
+                self.inc(x, y)
+            }
         }
     }
 
-    fn reset_flashed(&mut self, x: usize, y: usize) -> u32 {
-        let e = &mut self.grid[y][x];
-        let res = if e.flashed { 1 } else { 0 };
-        e.level = if e.flashed { 0 } else { e.level };
-        e.flashed = false;
+    fn reset_flashed(&mut self, x: isize, y: isize) -> u32 {
+        if let Some(e) = self.get_mut(x, y) {
+            let res = if e.flashed { 1 } else { 0 };
+            e.level = if e.flashed { 0 } else { e.level };
+            e.flashed = false;
 
-        res
-    }
-
-    fn inc(&mut self, x: usize, y: usize) {
-        let e = &mut self.grid[y][x];
-        e.level += 1;
-
-        if e.level > 9 && !e.flashed {
-            self.flash(x, y);
+            res
+        } else {
+            0
         }
     }
 
-    fn neighbors(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
-        let first_col = x == 0;
-        let last_col = x == self.width - 1;
-        let first_row = y == 0;
-        let last_row = y == self.height - 1;
+    fn inc(&mut self, x: isize, y: isize) {
+        if let Some(e) = self.get_mut(x, y) {
+            e.level += 1;
 
-        let left = (!first_col, (-1, 0));
-        let right = (!last_col, (1, 0));
-        let top = (!first_row, (0, -1));
-        let bottom = (!last_row, (0, 1));
-        let top_left = (!first_row && !first_col, (-1, -1));
-        let top_right = (!first_row && !last_col, (1, -1));
-        let bottom_left = (!last_row && !first_col, (-1, 1));
-        let bottom_right = (!last_row && !last_col, (1, 1));
+            if e.level > 9 && !e.flashed {
+                self.flash(x, y);
+            }
+        }
+    }
 
+    fn neighbors(&self, x: isize, y: isize) -> Vec<(isize, isize)> {
         [
-            left,
-            right,
-            top,
-            bottom,
-            top_left,
-            top_right,
-            bottom_left,
-            bottom_right,
+            (-1, 0),
+            (1, 0),
+            (0, -1),
+            (0, 1),
+            (-1, -1),
+            (1, -1),
+            (-1, 1),
+            (1, 1),
         ]
         .iter()
-        .filter_map(|(b, (dx, dy))| {
-            if *b {
-                Some(((x as isize + dx) as usize, (y as isize + dy) as usize))
-            } else {
-                None
-            }
-        })
+        .map(|(dx, dy)| ((x + dx), (y + dy)))
         .collect()
+    }
+
+    fn parse(lines: &[&str]) -> Self {
+        let width = lines[0].len();
+        let height = lines.len();
+        let grid = lines
+            .iter()
+            .map(|l| {
+                l.chars()
+                    .map(|c| Energy {
+                        level: c.to_digit(10).unwrap(),
+                        flashed: false,
+                    })
+                    .collect()
+            })
+            .collect();
+
+        Self {
+            width,
+            height,
+            grid,
+        }
+    }
+
+    fn get_mut(&mut self, x: isize, y: isize) -> Option<&mut Energy> {
+        let in_bounds =
+            x >= 0 && x as usize <= self.width - 1 && y >= 0 && y as usize <= self.height - 1;
+
+        if in_bounds {
+            Some(&mut self.grid[y as usize][x as usize])
+        } else {
+            None
+        }
     }
 }
 
 fn part1(lines: &[&str]) -> u32 {
+    let mut grid = Grid::parse(lines);
     let steps = 100;
-    let mut grid = Grid::new(lines.len(), lines[0].len());
-
-    for (y, line) in lines
-        .iter()
-        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()))
-        .enumerate()
-    {
-        for (x, cell) in line.enumerate() {
-            grid.grid[y][x] = Energy {
-                level: cell,
-                flashed: false,
-            }
-        }
-    }
 
     let mut res = 0;
     for _ in 0..steps {
@@ -133,10 +125,34 @@ fn part1(lines: &[&str]) -> u32 {
     res
 }
 
+fn part2(lines: &[&str]) -> u32 {
+    let mut grid = Grid::parse(lines);
+
+    let mut step = 0;
+    loop {
+        let all_zero = grid
+            .grid
+            .iter()
+            .flat_map(|row| row.iter().map(|e| e.level))
+            .all(|lvl| lvl == 0);
+
+        if all_zero {
+            break;
+        }
+
+        grid.step();
+        step += 1;
+    }
+
+    step
+}
+
 fn main() {
     let input_file: &str = include_str!("input.txt");
     let lines = input_file.lines().collect::<Vec<_>>();
 
     println!("Part 1:");
     println!("\t{}", part1(&lines));
+    println!("Part 2:");
+    println!("\t{}", part2(&lines));
 }
